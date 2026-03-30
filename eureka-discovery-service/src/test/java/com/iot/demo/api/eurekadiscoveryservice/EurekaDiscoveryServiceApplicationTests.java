a0ca07881d3678b46699742e8b198f0b34921c19
anupam1708 <anupam.1708@gmail.com>
2026-03-29 22:23:19 -0500

    fix: Upgrade Java 11 → 21 (LTS): applied OpenRewrite recipes (org.openrewrite.java.migrate.UpgradeToJava2

--- eureka-discovery-service/src/test/java/com/iot/demo/api/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
+++ eureka-discovery-service/src/test/java/com/iot/demo/api/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -3,7 +3,17 @@ package com.iot.demo.api.eurekadiscoveryservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"eureka.client.enabled=false",
+	"eureka.client.register-with-eureka=false",
+	"eureka.client.fetch-registry=false",
+	"spring.cloud.discovery.enabled=false",
+	"eureka.server.enableSelfPreservation=false",
+	"eureka.server.expectedClientRenewalIntervalSeconds=30",
+	"eureka.dashboard.enabled=false",
+	"server.port=0",
+	"spring.main.allow-bean-definition-overriding=true"
+}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class EurekaDiscoveryServiceApplicationTests
 {
 
